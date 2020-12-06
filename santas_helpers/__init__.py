43eeedea7d69332a3261a965ed2d1e6a26962827
Nathaniel Troutman <troutma@amazon.com>
2020-12-05 21:12:06 -0800

    Add new "group_on_blank" feature to load input in santas_helpers"

--- santas_helpers/__init__.py
+++ santas_helpers/__init__.py
@@ -1,20 +1,26 @@
-def get_input(day, force=False):
-    import requests
-    import os
-    
-    full = []
-    with open(f"./input/day{day}.txt", "rt") as input_file:
-        full = [line.strip() for line in input_file]
-            
-    example_1 = []
-    if os.path.exists(f"./input/day{day}-example1.txt"):
-        with open(f"./input/day{day}-example1.txt", "rt") as input_file:
-            example_1 = [line.strip() for line in input_file]
-            
-    example_2 = []
-    if os.path.exists(f"./input/day{day}-example2.txt"):
-        with open(f"./input/day{day}-example2.txt", "rt") as input_file:
-            example_2 = [line.strip() for line in input_file]
+import os
+
+def get_input(day, force=False, group_on_blank=False):    
+    example_1 = read_file(f"./input/day{day}-example1.txt", group_on_blank)
+    example_2 = read_file(f"./input/day{day}-example2.txt", group_on_blank)
+    full = read_file(f"./input/day{day}.txt", group_on_blank)
     
     return example_1, example_2, full
-          
+
+def read_file(filename, group_on_blank):
+    records = []
+    if os.path.exists(filename):
+        with open(filename, "rt") as input_file:
+            records = [line.strip() for line in input_file]
+        
+    if group_on_blank:
+        groups = [[]]
+        for record in records:
+            if record == '':
+                groups.append([])
+                continue
+            groups[-1].append(record)
+        records = groups
+    
+    return records
+
